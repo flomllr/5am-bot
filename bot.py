1dc16e7e58ed15c1eb30a958a8ffac364a468730
Berzan Yildiz <berzan.yildiz@tum.de>
2019-07-11 09:17:45 +0200

    Fixed bug with photo resend

--- bot.py
+++ bot.py
@@ -1,35 +1,33 @@
-# Imports
-import os
 import logging
-
-from os.path import join, dirname
 import config
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ContentType
 
-
-# Logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-# Methods
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when cliend sends '/start' or '/help' commands.
+    This handler will be called when client send `/start` or `/help` commands.
     """
-    print("Hello world")
-    await message.reply("I'm alive.")
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-# TODO: Make this message handler work
-@dp.message_handler(content_types=ContentType.PHOTO)
-async def echo(message: types.Message):
-    print(message)
-    await bot.send_message(message.chat.id, message.text)
+
+@dp.message_handler(content_types=[ContentType.PHOTO])
+async def cats(message: types.Message):
+    print("test")
+    raw = message.photo[2].file_id
+    path = raw+".png"
+    file_info = await bot.get_file(raw)
+    photo = await bot.download_file(file_info.file_path)
+    await bot.send_photo(message.chat.id, photo, caption='Nice pic!')
 
 
 if __name__ == '__main__':
