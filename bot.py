40991282d59e72269dc77d718e8548928538fcfb
Florian Mueller <contact@flomllr.com>
2019-07-11 06:42:05 +0200

    Remove js version and refactor

--- bot.py
+++ bot.py
@@ -1,27 +1,35 @@
-# Setup .env
+# Imports
 import os
+import logging
+
 from os.path import join, dirname
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+
+# Setup
+# .env
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
-# Setup logging
-import logging
+# Logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-# Setup telegram bot
-from telegram.ext import Updater
+                    level=logging.INFO)
+# Bot
 token = os.getenv('TOKEN')
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
-# First command
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-# Setup command handler
-from telegram.ext import CommandHandler
+# Functions
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+# Command handler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+
+# Start the bot
 updater.start_polling()
